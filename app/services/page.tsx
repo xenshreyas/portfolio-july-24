6941739109b7a9efdaca5f8e7e3ecf3e188a318e
xenshreyas <xenshreyas@gmail.com>
2024-07-06 23:07:37 -0700

    update import cap

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { BsArrowDownRight } from "React-icons/bs";
+import { BsArrowDownRight } from "react-icons/bs";
 import { motion } from "framer-motion";
 import Link from "next/link";
 
